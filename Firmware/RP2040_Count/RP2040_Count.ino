5830a9e8dc951ffe172d3f378e6ddfdf50cabdca
Scott Hanson <scooter_seh@yahoo.com>
2024-03-03 01:29:21 -0500

    added lies

---
+++ Firmware/RP2040_Count/RP2040_Count.ino
@@ -0,0 +1,222 @@
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+#include <Adafruit_INA219.h>
+#include <Adafruit_NeoPixel.h>
+
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+#define OLED_RESET     -1 // Reset pin # (or -1 if sharing Arduino reset pin)
+#define SCREEN_ADDRESS 0x3C  // Address 0x3C default
+
+Adafruit_SSD1306 display = Adafruit_SSD1306(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire1, OLED_RESET);
+Adafruit_INA219 ina219;
+
+#define PIXEL_PIN    14  // Digital IO pin connected to the NeoPixels.
+#define PIXEL_COUNT 300  // Number of NeoPixels
+
+// Declare our NeoPixel strip object:
+Adafruit_NeoPixel strip(PIXEL_COUNT, PIXEL_PIN, NEO_RGB + NEO_KHZ800);
+
+#define BUTTON_A  13
+
+#define PIXEL_CURRENT_DIFF 5
+#define CURRENT_READ_DELAY 5
+
+boolean oldState = HIGH;
+uint32_t pixel_count = 0;
+uint16_t color = 0;
+
+void setup() {
+  Serial.begin(115200);
+  //delay(3000);//serial debug doesnt work without this
+  Serial.println("Starting Pixel Tester");
+  Wire1.setSDA(10);
+  Wire1.setSCL(11);
+  Serial.println("INA219 begin");
+  if (!ina219.begin(&Wire1)) {
+    Serial.println("Failed to find INA219 chip");
+    while (1) { delay(10); }
+  }
+  pinMode(BUTTON_A, INPUT_PULLUP);
+  strip.begin(); // Initialize NeoPixel strip object (REQUIRED)
+  strip.show();  // Initialize all pixels to 'off'
+
+  Serial.println("OLED begin");
+  delay(250); // wait for the OLED to power up
+  display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS); // Address 0x3C default
+
+  display.setRotation(2);
+  // Show image buffer on the display hardware.
+  // Since the buffer is intialized with an Adafruit splashscreen
+  // internally, this will display the splashscreen.
+  //display.display();
+  //delay(1000);
+
+  // Clear the buffer.
+  display.clearDisplay();
+  display.display();
+
+  // text display tests
+  display.setTextSize(2);
+  display.setTextColor(SSD1306_WHITE);
+  display.setCursor(0,0);
+
+  display.display(); // actually display all of the above
+}
+
+void loop() {
+  boolean newState = digitalRead(BUTTON_A);
+
+  // Check if state changed from high to low (button press).
+  if((newState == LOW) && (oldState == HIGH)) {
+    // Short delay to debounce button.
+    delay(20);
+    // Check if button is still low after debounce.
+    newState = digitalRead(BUTTON_A);
+    if(newState == LOW) { 
+      for(int i = 0; i< strip.numPixels(); i++) {
+        strip.setPixelColor(i, strip.Color(0,0,0));  
+      }
+      strip.show();  
+      delay(50);
+
+      float off_current = ina219.getCurrent_mA(); //55ma on a 50pix bullet string all off
+
+      for(int ledidx = 0; ledidx < strip.numPixels(); ledidx++) { 
+        for(int dot = 0; dot < strip.numPixels(); dot++) {
+          if(dot == ledidx) {
+            strip.setPixelColor(dot, strip.Color(255,255,255));
+          }
+          else {
+            strip.setPixelColor(dot, strip.Color(0,0,0));
+          }
+        }
+        strip.show();
+        delay(CURRENT_READ_DELAY);
+        yield();
+
+        float on_current = ina219.getCurrent_mA(); //100ma on on 50pix bullet string with one on
+        update_power_display(ledidx + 1);
+        display.display();
+
+        if((on_current - off_current) < PIXEL_CURRENT_DIFF) {
+          pixel_count = ledidx;
+          break;
+        }
+      }
+    }
+  }
+
+  // Set the last-read button state to the old state.
+  oldState = newState;
+  color++;
+  if(color > 255) {
+    color = 0;
+  }
+  rainbowCycle(color);
+  delay(10);
+  yield();
+  update_power_display(pixel_count);
+  display.display();
+}
+
+void update_power_display(int pixelNum) {
+  // Read voltage and current from INA219.
+  float shuntvoltage = ina219.getShuntVoltage_mV();
+  float busvoltage = ina219.getBusVoltage_V();
+  float current_mA = ina219.getCurrent_mA();
+
+  // Compute load voltage
+  float loadvoltage = busvoltage + (shuntvoltage / 1000);
+  
+  // Update display.
+  display.clearDisplay();
+  display.setCursor(0, 0);
+  display.print("Pixels:");
+  display.print(pixelNum);
+
+  display.setCursor(0, 16);
+
+  // Mode 0, display volts and amps.
+  printSIValue(loadvoltage, "V:", 2, 10);
+  display.setCursor(0, 32);
+  printSIValue(current_mA/1000.0, "A:", 5, 10);
+
+  display.display();
+}
+
+void printSIValue(float value, const char* units, int precision, int maxWidth) {
+  // Print a value in SI units with the units left justified and value right justified.
+  // Will switch to milli prefix if value is below 1.
+
+  // Add milli prefix if low value.
+  if (fabs(value) < 1.0) {
+    display.print('m');
+    maxWidth -= 1;
+    value *= 1000.0;
+    precision = max(0, precision-3);
+  }
+
+  // Print units.
+  display.print(units);
+  maxWidth -= strlen(units);
+
+  // Leave room for negative sign if value is negative.
+  if (value < 0.0) {
+    maxWidth -= 1;
+  }
+
+  // Find how many digits are in value.
+  int digits = ceil(log10(fabs(value)));
+  if (fabs(value) < 1.0) {
+    digits = 1; // Leave room for 0 when value is below 0.
+  }
+
+  // Handle if not enough width to display value, just print dashes.
+  if (digits > maxWidth) {
+    // Fill width with dashes (and add extra dash for negative values);
+    for (int i=0; i < maxWidth; ++i) {
+      display.print('-');
+    }
+    if (value < 0.0) {
+      display.print('-');
+    }
+    return;
+  }
+
+  // Compute actual precision for printed value based on space left after
+  // printing digits and decimal point.  Clamp within 0 to desired precision.
+  int actualPrecision = constrain(maxWidth-digits-1, 0, precision);
+
+  // Compute how much padding to add to right justify.
+  int padding = maxWidth-digits-1-actualPrecision;
+  for (int i=0; i < padding; ++i) {
+    display.print(' ');
+  }
+
+  // Finally, print the value!
+  display.print(value, actualPrecision);
+}
+
+void rainbowCycle(uint16_t color) {
+    for(uint16_t i=0; i< strip.numPixels(); i++) {
+      strip.setPixelColor(i, Wheel(((i * 256 / strip.numPixels()) + color) & 255));
+    }
+    strip.show();
+}
+
+// Input a value 0 to 255 to get a color value.
+// The colours are a transition r - g - b - back to r.
+uint32_t Wheel(byte WheelPos) {
+  WheelPos = 255 - WheelPos;
+  if(WheelPos < 85) {
+    return strip.Color(255 - WheelPos * 3, 0, WheelPos * 3);
+  }
+  if(WheelPos < 170) {
+    WheelPos -= 85;
+    return strip.Color(0, WheelPos * 3, 255 - WheelPos * 3);
+  }
+  WheelPos -= 170;
+  return strip.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
+}
--- README.md
+++ README.md
@@ -1,2 +1,20 @@
-# RP2040_Count
-RP2040 Pixel Counter
+# Copyright and Disclaimer
+Copyright: Scott Hanson
+
+This documentation describes Open Hardware and is licensed under the CERN Open Hardware License Version 2 - Strongly Reciprocal. (CERN-OHL-S)
+
+You may redistribute and modify this documentation under the terms of the CERN OHL-S-v2 (https://ohwr.org/cern_ohl_s_v2.txt). This documentation is distributed WITHOUT ANY EXPRESS OR IMPLIED WARRANTY, INCLUDING OF MERCHANTABILITY, SATISFACTORY QUALITY AND FITNESS FOR A PARTICULAR PURPOSE. Please see the CERN OHL v2-S for applicable conditions
+
+**Under CERN OHL-S-v2, derivative work must be publicly released as defined in subsection 3.3**
+
+# RP2040 Count
+
+RP2040 Pixel Counter is a 'pocket' Pixel Counter. All the design files are open source and available on github.
+
+To order PCBs upload the ["GERBER-BBB_16.zip"](https://github.com/computergeek1507/RP2040_Count/raw/master/PCB/jlcpcb/production_files/GERBER-RP2040_Count.zip) file in the "jlcpcb\production_files" folder to jlcpcb.com.
+
+[^Comment]:  ## [Part BOM](https://github.com/computergeek1507/RP2040_Count/raw/master/PCB/RP2040_Count_BOM.ods)
+
+## [Interactive BOM](https://computergeek1507.github.io/RP2040_Count/PCB/bom/ibom)
+
+![Image of RP2040 Count](https://github.com/computergeek1507/RP2040_Count/raw/master/PCB/RP2040_Count.png)
